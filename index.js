42bc8f90e28fd0f5c1c36311ff2232a835460431
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-09-21 12:16:14 -0400

    commit tree e688ed8cd68e97b8bb3c760b73ec799d3ef8425c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+const dodger = document.getElementById("dodger")
+
+dodger.style.backgroundColor = "olive"
+
+dodger.style.bottom = '0px'
+// dodger.style.left = '0px'
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft()
+    }
+})
+
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`
+    }
+    
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+})
